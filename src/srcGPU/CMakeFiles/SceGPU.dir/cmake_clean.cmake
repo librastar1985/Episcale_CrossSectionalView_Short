e0578ac5692c1bf623f1276ceaf814089edac95a
Kevin Tsai <ytsai003@ucr.edu>
2021-09-03 13:42:22 -0700

    Add files via upload

---
+++ src/srcGPU/CMakeFiles/SceGPU.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SceGPU.dir/SceGPU_generated_SceCells.cu.o"
+  "CMakeFiles/SceGPU.dir/SceGPU_generated_SceECM.cu.o"
+  "CMakeFiles/SceGPU.dir/SceGPU_generated_SceNodes.cu.o"
+  "CMakeFiles/SceGPU.dir/SceGPU_generated_SimulationDomainGPU.cu.o"
+  "CMakeFiles/SceGPU.dir/SceGPU_generated_Solver.cu.o"
+  "libSceGPU.a"
+  "libSceGPU.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/SceGPU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/srcGPU/CMakeFiles/SceGPU.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SceGPU.
+# This may be replaced when dependencies are built.
